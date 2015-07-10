b71581b0e79961c59ed2b2f8ba99155850d74f74
willrees <william.rees@bondbrandloyalty.com>
2015-07-10 12:32:36 -0400

    updates

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+﻿module.exports = function (grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+       
+        concat: {
+            dist: {
+                src: [                    
+                    'scripts/src/dm.border.js',                    
+                ],
+                dest: 'scripts/dist/dm.border.js',
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'scripts/dist/dm.border.min.js': 'scripts/dist/dm.border.js'
+                }
+            }
+        },               
+        watch: {
+            scripts: {
+                files: ['scripts/**/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                },
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');        
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['less','concat', 'uglify']);
+};
---
+++ scripts/dist/dm.border.js
@@ -0,0 +1,59 @@
+(function($) {
+	dm.componentFactory("border", function($) {
+		var border = function (options) {
+			var defaults = {
+				borderColor: "#000",
+				borderWidth: "2px",
+				borderStyle: "solid"
+			};
+			
+			var plugin = this;
+			plugin.$el = $(this.selector);
+			plugin.settings = jQuery.extend(true, defaults, options);
+			
+			plugin.$el.each(function() {				
+				$(this).css({
+					"border-color": plugin.settings.borderColor,
+					"border-width": plugin.settings.borderWidth,
+					"border-style": plugin.settings.borderStyle
+				});
+			});
+			
+			plugin.isBlinking = false;
+			plugin.blinkInterval;
+			
+			plugin.blink = function () {
+				var $el = plugin.$el;
+				var state = "on";
+				
+				if (plugin.isBlinking === false) {
+					plugin.isBlinking = true;
+					blink();
+				
+					plugin.blinkInterval = setInterval(function () {
+						blink();
+					}, 500);
+					
+					function blink() {
+						if (state === "on") {
+							state = "off";
+							$el.css('border-color', 'transparent');
+						} else {
+							state = "on";
+							$el.css('border-color', plugin.settings.borderColor);
+						}
+					}
+				} else {
+					plugin.isBlinking = false;
+					clearInterval(plugin.blinkInterval);
+					$el.css('border-color', plugin.settings.borderColor);
+				}
+			};
+			
+			return plugin;
+		};
+		
+		return border;
+		
+	}, null, jQuery);
+})(jQuery);
